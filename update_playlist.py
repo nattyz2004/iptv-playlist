457085ea7124feccc5376396a08a476b39985326
nattyz2004 <nattyz2004@yahoo.com>
2026-03-07 18:16:56 -0700

    Update update_playlist.py

--- update_playlist.py
+++ update_playlist.py
@@ -1,23 +1,27 @@
 import json
 import subprocess
 
-with open("channels.json") as f:
+with open("channels.json", "r", encoding="utf-8") as f:
     channels = json.load(f)
 
 playlist = "#EXTM3U\n\n"
 
 for ch in channels:
-    try:
-        url = subprocess.check_output(
-            ["yt-dlp", "-g", ch["source"]],
-            text=True
-        ).strip()
+    source = ch["source"]
 
-        playlist += f'#EXTINF:-1 group-title="{ch["group"]}",{ch["name"]}\n'
-        playlist += url + "\n\n"
+    if source.endswith(".m3u8"):
+        url = source
+    else:
+        try:
+            url = subprocess.check_output(
+                ["yt-dlp", "-g", source],
+                text=True
+            ).strip()
+        except Exception:
+            continue
 
-    except:
-        pass
+    playlist += f'#EXTINF:-1 group-title="{ch["group"]}",{ch["name"]}\n'
+    playlist += url + "\n\n"
 
-with open("playlist.m3u", "w") as f:
+with open("playlist.m3u", "w", encoding="utf-8") as f:
     f.write(playlist)
